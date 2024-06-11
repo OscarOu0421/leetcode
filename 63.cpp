6afe7a59b390d9f57a14aa7a9c46737aa75ddaf1
Oscar Ou <hhh860421@gmail.com>
2024-06-11 23:02:02 +0800

    63. Unique Paths II
    Signed-off-by: Oscar Ou <hhh860421@gmail.com>

---
+++ 63.cpp
@@ -0,0 +1,24 @@
+// Time: O(mn)
+// Space: O(mn)
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j]) {
+                    dp[i][j] = 0;
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    int left = j-1 >= 0 ? dp[i][j-1] : 0;
+                    int up  = i-1 >= 0 ? dp[i-1][j] : 0;
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
