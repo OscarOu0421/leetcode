668f87eced8e679f6ab39de2e04ee03a9f482f9f
Oscar Ou <hhh860421@gmail.com>
2025-03-12 21:24:34 +0800

    104. Maximum Depth of Binary Tree
    modify
    
    Signed-off-by: Oscar Ou <hhh860421@gmail.com>

--- 104.cpp
+++ 104.cpp
@@ -4,25 +4,16 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-// Time: O(n), n is the number of nodes
-// Space: O(1)
+// TC: O(n), Where n is the total number of nodes in the tree.
+// SC: O(h) — Where h is the height of the tree
 class Solution {
 public:
-    int preorder(TreeNode *root, int depth) {
-        if (!root)
-            return depth;
-        
-        depth++;
-        int left = preorder(root->left, depth);
-        int right = preorder(root->right, depth);
-        return (left > right) ? left : right;
-    }
     int maxDepth(TreeNode* root) {
-        return preorder(root, 0);
+        if (!root)
+            return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
