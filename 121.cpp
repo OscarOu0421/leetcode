e55f94cbd47a39947714d1f431deda946d090297
Oscar Ou <hhh860421@gmail.com>
2025-03-12 21:55:17 +0800

    121. Best Time to Buy and Sell Stock
    modify
    
    Signed-off-by: Oscar Ou <hhh860421@gmail.com>

--- 121.cpp
+++ 121.cpp
@@ -3,25 +3,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minP = prices[0];
-        int maxP = prices[0];
-        int ret = 0;
+        int minP = INT_MAX;
+        int maxP = 0;
 
-        for (auto p : prices) {
-            /*
-             * These price is min, so you could buy it,
-             * and you need to update the sell price, because you could not sell 
-             * this stuck before.
-             */
-            if (p < minP) {
-                maxP = p;
-                minP = p;
-            }
-            if (p > maxP) {
-                maxP = p;
-            }
-            ret = max(ret, maxP - minP);
+        for (auto price : prices) {
+            minP = min(price, minP);
+            maxP = max(maxP, price - minP);
         }
-        return ret;
+        return maxP;
     }
 };
