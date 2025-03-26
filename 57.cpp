61331ae48acec5418fa70008c4815487a53c6cae
Oscar Ou <hhh860421@gmail.com>
2025-03-26 23:46:34 +0800

    57. Insert Interval
    Signed-off-by: Oscar Ou <hhh860421@gmail.com>

--- 57.cpp
+++ 57.cpp
@@ -1,29 +1,32 @@
-// Time: O(n)
-// Space: O(1)
+// TC: O(n)
+// SC: O(n)
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> ret;
+        vector<vector<int>> result;
+        int i = 0;
+        int n = intervals.size();
 
-        for (auto interval : intervals) {
-            if (newInterval[0] <= interval[1]) {
-                // non-overlap for new interval
-                if (newInterval[1] < interval[0]) {
-                    ret.push_back(newInterval);
-                    newInterval[0] = INT_MAX;
-                }
-                // overlap for new interval
-                else {
-                    newInterval[0] = min(interval[0], newInterval[0]);
-                    newInterval[1] = max(interval[1], newInterval[1]);
-                    continue;
-                }
-            }
-            // non-overlap before new interval
-            ret.push_back(interval);
+        // Step 1: Add all intervals ending before newInterval starts
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.push_back(intervals[i]);
+            i++;
         }
-        if (newInterval[0] != INT_MAX)
-            ret.push_back(newInterval);
-        return ret;
+
+        // Step 2: Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.push_back(newInterval);
+
+        // Step 3: Add remaining intervals starting after newInterval ends
+        while (i < n) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+        return result;
     }
 };
