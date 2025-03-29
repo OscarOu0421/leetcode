846e9bd894c7e03a71b83bbd396bfc72655aae2e
Oscar Ou <hhh860421@gmail.com>
2025-03-29 12:21:30 +0800

    63. Unique Paths II
    Signed-off-by: Oscar Ou <hhh860421@gmail.com>

--- 63.cpp
+++ 63.cpp
@@ -1,24 +1,41 @@
-// Time: O(mn)
-// Space: O(mn)
+// Time Complexity: O(m * n) 
+// Space Complexity: O(m * n) 
+// DP Formula: dp[i][j] = (obstacleGrid[i][j] == 1) ? 0 : dp[i-1][j] + dp[i][j-1]
+
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        
+        // DP array initialized with 0
         vector<vector<int>> dp(m, vector<int>(n, 0));
 
+        // Traverse the grid
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                
+                // If there's an obstacle, no paths can go through this cell
                 if (obstacleGrid[i][j]) {
                     dp[i][j] = 0;
-                } else if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    int left = j-1 >= 0 ? dp[i][j-1] : 0;
-                    int up  = i-1 >= 0 ? dp[i-1][j] : 0;
-                    dp[i][j] = up + left;
+                    continue;
                 }
+
+                // Starting cell initialization
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                // Collect paths from top and left cells (if within bounds)
+                int top = j - 1 >= 0 ? dp[i][j-1] : 0;
+                int left = i - 1 >= 0 ? dp[i-1][j] : 0;
+
+                dp[i][j] = top + left;
             }
         }
+
+        // Return the number of unique paths to the bottom-right corner
         return dp[m-1][n-1];
     }
 };
