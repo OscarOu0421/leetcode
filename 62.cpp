334cf9bf7543a3384d6f1732a0d8b97530f8608f
Oscar Ou <hhh860421@gmail.com>
2025-03-29 12:11:58 +0800

    62. Unique Paths
    Signed-off-by: Oscar Ou <hhh860421@gmail.com>

--- 62.cpp
+++ 62.cpp
@@ -1,21 +1,21 @@
-// Time: O(mn)
-// Space: O(mn)
+// Time Complexity: O(m * n)
+// Space Complexity: O(m * n)
+// DP Formula: dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        // Initialize DP table with 1s since there's only one way to reach cells in the first row/column
+        vector<vector<int>> dp(m, vector<int>(n, 1));
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0)
-                    dp[i][j] = 1;
-                else {
-                    int left = j-1 >= 0 ? dp[i][j-1] : 0;
-                    int up = i-1 >= 0 ? dp[i-1][j] : 0;
-                    dp[i][j] = left + up;
-                }
+        // Fill DP table using the recurrence relation
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
+
+        // Return the number of unique paths to the bottom-right corner
         return dp[m-1][n-1];
     }
 };
